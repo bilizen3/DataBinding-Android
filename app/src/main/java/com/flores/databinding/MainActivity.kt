038ad6dacab21a70e0defd29941308448f402141
Bill Flores <bilizen3@gmail.com>
2019-04-22 01:02:13 -0500

    config databinding :arrow_up:

--- app/src/main/java/com/flores/databinding/MainActivity.kt
+++ app/src/main/java/com/flores/databinding/MainActivity.kt
@@ -2,11 +2,17 @@ package com.flores.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.flores.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val user = User()
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.user=user
     }
+
 }
---
+++ app/src/main/java/com/flores/databinding/User.kt
@@ -0,0 +1,8 @@
+package com.flores.databinding
+
+data class User(
+    val name: String ="Bill",
+    val dni: Int=999999,
+    val phone: String="5555555",
+    val address: String="Av. San juan 4564"
+)
